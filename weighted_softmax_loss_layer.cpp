cee8285c8d9818435f8327551b50795100755fcb
Rex <holyrexcheng@gmail.com>
2018-01-20 15:04:10 +0800

    adapt to new caffe version

--- weighted_softmax_loss_layer.cpp
+++ weighted_softmax_loss_layer.cpp
@@ -2,10 +2,8 @@
 #include <cfloat>
 #include <vector>
 
-#include "caffe/layer.hpp"
-#include "caffe/layer_factory.hpp"
+#include "caffe/layers/weighted_softmax_loss_layer.hpp"
 #include "caffe/util/math_functions.hpp"
-#include "caffe/vision_layers.hpp"
 
 namespace caffe {
 
